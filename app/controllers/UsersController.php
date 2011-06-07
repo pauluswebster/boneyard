61409c2f122a7f9ec72e4a72751b1b0029346a2d
Paul Webster <paul@webprogression.co.nz>
2011-06-07 20:36:43 +1200

    Scaffold delete action inline

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -8,6 +8,9 @@
  
 namespace app\controllers;
 
+use slicedup_core\action\FlashMessage;
+use slicedup_scaffold\core\Scaffold;
+
 class UsersController extends \slicedup_users\controllers\UsersController {
 	
 	public $scaffold;
@@ -17,7 +20,14 @@ class UsersController extends \slicedup_users\controllers\UsersController {
 		parent::_init();
 	}
 	
-	public function update(){}
+	public function edit(){
+		if (!$this->_user->admin && $this->request->id != $this->_user->id) {
+			FlashMessage::error('Permision Denied. <sup>[U25]</sup>');
+			return $this->redirect('/');
+		}
+		$this->_render['hasRendered'] = true;
+		Scaffold::invoke($this);
+	}
 }
 
 ?>
