62fd3403bd232e377b37b8197ad755d1118b4b58
Paul Webster <paul@webprogression.co.nz>
2011-06-09 22:46:22 +1200

    Add user edit ajax template

---
+++ app/views/users/edit.ajax.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Slicedup: a fancy tag line here
+ *
+ * @copyright	Copyright 2011, Paul Webster / Slicedup (http://slicedup.org)
+ * @license 	http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+ 
+
+?>
+
+<div class="<?php echo $plural;?>">
+	<div class="edit <?php echo $singular;?>">
+		<?php
+			echo $this->form->create($record, array('url' => $this->_request->url));
+		?>
+		<h2>Account Settings</h2>
+		<?php
+			echo $this->form->field('first_name');
+			echo $this->form->field('last_name');
+			echo $this->form->field('username');
+			echo $this->form->field('email');
+			echo $this->form->field('new_password', array('type' => 'password', 'label' => 'Change Password'));
+			echo $this->form->end();
+		?>
+	</div>
+</div>
