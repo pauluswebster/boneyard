8302c103c4790cd84b406f2d023ad16146558c49
Paul Webster <paul@webprogression.co.nz>
2012-02-03 22:02:33 +1300

    Add: Environment - config sample & ignore

---
+++ environment.sample.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Enviroment config & credentials
+ * 
+ * Create a copy of this file as `environment.php` and configure
+ */
+$config = array(
+	'connections' => array(
+		'default' => array(
+			'login' => '',
+ 			'password' => '',
+ 			'database' => ''
+		)
+	),
+	'hosts' => array(
+		'service' => '',
+		'default' => ''
+	),
+	'analytics' => array(
+		'service' => '',
+		'default' => ''
+	)
+);
+
+?>
