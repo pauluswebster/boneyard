e017f86d3ceb9a8ea2a21f88379aa41d7575da4e
Paul Webster <paul@webprogression.co.nz>
2012-02-28 21:45:02 +1300

    Change: use new service helper for recommended links

--- app/views/elements/layout/sidebar/recommends.html.php
+++ app/views/elements/layout/sidebar/recommends.html.php
@@ -1,18 +1,18 @@
 <section class='widget-container widget-text'>
 	<h2 class='widget-heading'>p.m.d. recommends:</h2>
 	<div class='widget-content recommends'>
-		<a href="recommends/servergrove" title="Server Grove: VPS hosting">
+		<a href="<?php echo $this->service->url('servergrove');?>" title="Server Grove: VPS hosting">
 			<img src="/img/site/sg.png" width="200" />
 		</a>
 		<div class="clear"></div>
-		<a href="recommends/hostingdirect" title="Hosting Direct: web hosting & domain name services (NZ)">
+		<a href="<?php echo $this->service->url('hostingdirect');?>" title="Hosting Direct: web hosting & domain name services (NZ)">
 			<img src="/img/site/hd.png" width="200" />
 		</a>
 		<div class="clear"></div>
-		<a href="recommends/traffictravis" title="Traffic Travis: desktop seo software">
+		<a href="<?php echo $this->service->url('traffictravis');?>" title="Traffic Travis: desktop seo software">
  			<img src="/img/site/tt.png" width="100" />
 		</a>
-		<a href="recommends/gittower" title="Tower: The most powerful Git client for Mac">
+		<a href="<?php echo $this->service->url('gittower');?>" title="Tower: The most powerful Git client for Mac">
 			<img src="/img/site/tower.png" width="80" />
 		</a>
 		<div class="clear"></div>
