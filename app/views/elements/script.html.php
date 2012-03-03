d831d3a7351a8f746ca66535e702dfcfc731de87
Paul Webster <paul@webprogression.co.nz>
2012-03-04 11:05:06 +1300

    Change: script and style updates to implement chose on select fields

--- app/views/elements/script.html.php
+++ app/views/elements/script.html.php
@@ -6,15 +6,17 @@
  * @license 	http://opensource.org/licenses/bsd-license.php The BSD License
  */
 echo $this->html->style(array(
+	'mootools-datepicker',
+	'chosen',
 	'app',
-	'fonts',
-	'mootools-datepicker'
+	'fonts'
 ));
 
 echo $this->html->script(array(
-	'mootools-core-1.4.0-full-nocompat-yc.js',
-	'mootools-more-1.4.0.1-nocompat-yc.js',
+	'mootools-core-1.4.5.js',
+	'mootools-more-1.4.0.1.js',
 	'mootools-datepicker-yc.js',
+	'chosen.min.js',
 	'app'
 ));
 
