79306f02e953f1f25018a1410eb29ce749b4509c
Paul Webster <paul@webprogression.co.nz>
2012-04-19 14:29:35 +1200

    Change: IMport actions to debug staging env

--- libraries/tmimport/controllers/ImportController.php
+++ libraries/tmimport/controllers/ImportController.php
@@ -7,12 +7,25 @@ use tmimport\models\TMCategories;
 
 class ImportController extends \lithium\action\Controller {
 		
-	public function index() {
-//		TMListings::importRecords();
+	public function query() {
 		if (!empty($this->request->args)) {
 			$query = reset($this->request->args);
+			echo "Importing {$query}";
 			TMListings::importData($query);
-		}	
+		}
+		die;
+	}
+	
+	public function setup() {
+		$path = LITHIUM_APP_PATH . '/resources/Used';
+		@mkdir($path, 0777, true);
+		echo "path exists: " . is_dir($path);
+		echo "path is writable: " . is_writable($path);
+		die;
+	}
+	
+	public function import() {
+		TMListings::importRecords();
 		die;
 	}
 }
