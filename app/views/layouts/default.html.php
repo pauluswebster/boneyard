976161193f0245fd0226472933f8f84e91892172
Paul Webster <paul@webprogression.co.nz>
2012-04-19 14:23:59 +1200

    Change: Template Scripts - extract to own element

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -26,26 +26,7 @@
 		<hr>
 		&copy; <?php echo date('Y')?>
 	</footer>
-	<script>
-		yepnope({
-			load: [
-				'/js/jquery.min.js',
-//				'/js/bootstrap/bootstrap-transition.js',
-				'/js/bootstrap/bootstrap-alert.js',
-//				'/js/bootstrap/bootstrap-modal.js',
-//				'/js/bootstrap/bootstrap-dropdown.js',
-//				'/js/bootstrap/bootstrap-scrollspy.js',
-				'/js/bootstrap/bootstrap-tab.js',
-//				'/js/bootstrap/bootstrap-tooltip.js',
-//				'/js/bootstrap/bootstrap-popover.js',
-//				'/js/bootstrap/bootstrap-button.js',
-//				'/js/bootstrap/bootstrap-collapse.js',
-//				'/js/bootstrap/bootstrap-carousel.js',
-//				'/js/bootstrap/bootstrap-typeahead.js',
-				'/js/app.js'
-			]
-		});
-	</script>
+	<?php echo $this->element->render('scripts');?>
 	<?php echo $this->element->render('debug/connection');?>
 </body>
 </html>
