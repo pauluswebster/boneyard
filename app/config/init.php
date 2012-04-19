ee9b5d188774f6ec56979935ae3a642c7e1550f1
Paul Webster <paul@webprogression.co.nz>
2012-04-19 13:58:55 +1200

    Change: Staging control - only apply staging user checking in staging environment

--- app/config/init.php
+++ app/config/init.php
@@ -1,20 +1,26 @@
 <?php
 
 use lithium\action\Dispatcher;
+use lithium\core\Environment;
 use sli_users\security\User;
 use sli_base\action\FlashMessage;
 
 /**
- * Enforce staging user
+ * Apply dispatch filters
  */
 Dispatcher::applyFilter('_call', function($self, $params, $chain) {
-	$controller = $params['callable'];
-	$user =& User::instance('staging');
-	if (!$user->get() && $controller->request->url != 'staging/login') {
-		$required = $user->required($controller);
-		if ($required instanceOf \lithium\action\Response) {
-			FlashMessage::write(array('message' => 'Please login.', 'class' => 'nofade'));
-			return $required;
+	if (Environment::is('staging')) {
+		/*
+		 * Enforce staging user
+		 */
+		$controller = $params['callable'];
+		$user =& User::instance('staging');
+		if (!$user->get() && $controller->request->url != 'staging/login') {
+			$required = $user->required($controller);
+			if ($required instanceOf \lithium\action\Response) {
+				FlashMessage::write(array('message' => 'Please login.', 'class' => 'nofade'));
+				return $required;
+			}
 		}
 	}
 	return $chain->next($self, $params, $chain);
