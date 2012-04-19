dd28d03e2c679ef69c07f6bea6dc513745254299
Paul Webster <paul@webprogression.co.nz>
2012-04-19 13:51:40 +1200

    Fix: Layout Scripts - use correct paths, enable alert script

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -30,18 +30,18 @@
 		yepnope({
 			load: [
 				'/js/jquery.min.js',
-//				'/js/bootstrap-transition.js',
-//				'/js/bootstrap-alert.js',
-//				'/js/bootstrap-modal.js',
-//				'/js/bootstrap-dropdown.js',
-//				'/js/bootstrap-scrollspy.js',
+//				'/js/bootstrap/bootstrap-transition.js',
+				'/js/bootstrap/bootstrap-alert.js',
+//				'/js/bootstrap/bootstrap-modal.js',
+//				'/js/bootstrap/bootstrap-dropdown.js',
+//				'/js/bootstrap/bootstrap-scrollspy.js',
 				'/js/bootstrap/bootstrap-tab.js',
-//				'/js/bootstrap-tooltip.js',
-//				'/js/bootstrap-popover.js',
-//				'/js/bootstrap-button.js',
-//				'/js/bootstrap-collapse.js',
-//				'/js/bootstrap-carousel.js',
-//				'/js/bootstrap-typeahead.js',
+//				'/js/bootstrap/bootstrap-tooltip.js',
+//				'/js/bootstrap/bootstrap-popover.js',
+//				'/js/bootstrap/bootstrap-button.js',
+//				'/js/bootstrap/bootstrap-collapse.js',
+//				'/js/bootstrap/bootstrap-carousel.js',
+//				'/js/bootstrap/bootstrap-typeahead.js',
 				'/js/app.js'
 			]
 		});
