77cdea49bb4047cc50a6f5c8c5b9bb822103182b
Paul Webster <paul@webprogression.co.nz>
2013-06-05 14:20:43 +1200

    Models - Change: Intermediate patch support for model::__init on app model

--- app/models/AppModel.php
+++ app/models/AppModel.php
@@ -5,10 +5,10 @@ namespace app\models;
 class AppModel extends \lithium\data\Model {
 
 	public static function __init() {
-		static::_isBase(__CLASS__, true);
-		parent::__init();
+		//static::_isBase(__CLASS__, true);
+		//parent::__init();
 	}
-	
+
 }
 
 ?>
